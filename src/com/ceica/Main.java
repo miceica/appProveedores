5df8c79d80f48b020818129bb6d86bea19f65743
miceica <miguelceica23@gmail.com>
2024-01-16 17:36:10 +0100

    Creamos AlmacenController y comprobamos en Main que funcione

--- src/com/ceica/Main.java
+++ src/com/ceica/Main.java
@@ -1,7 +1,11 @@
 package com.ceica;
 
+import com.ceica.Controladores.AlmacenController;
+
 public class Main {
     public static void main(String[] args) {
-        //AppProveedores
+        AlmacenController almacen = new AlmacenController();
+        almacen.nuevoProveedor("cif","nom","dir","loc","pro");
+        System.out.println(almacen.toString());
     }
 }
